69e7436f930bd8277a240d5db75ad0a8928c6c28
run <a936381813@163.com>
2025-07-29 10:15:36 +0800

    refactor: code

--- src/extension.ts
+++ src/extension.ts
@@ -20,6 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "git-commit-ai" is now active!');
+	console.log('Registering command: git-commit-ai.generateCommitMessage');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
